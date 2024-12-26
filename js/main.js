2c6efc72d1a62d72899f0c98ab04d45acd129bab
erkinovasrorbek001 <erkinovasrorbek3@gmail.com>
2024-12-26 18:32:58 +0500

    travel commit

---
+++ js/main.js
@@ -0,0 +1,503 @@
+//1-masala
+//let a = prompt ("a sonini kiriting");
+//let b = prompt ("b sonini kiriting");
+
+//S = a * b; 
+//P = 2 * (a + b);
+
+//console.log (S, P);
+
+//2-masala
+ 
+//let d = prompt ("aylanani deometrini kiriting");
+//P = 3;
+//R = d/2;
+//L = P+d
+//S = P+Math.pow(R, 2)
+//console.log (L, S);
+
+//3-masala
+
+//let a = prompt("a sonini kiriting");
+
+//V = Math.pow(a,3);
+//S = 6* Math.pow(a,2);
+
+//console.log(V,S);
+
+//4-masala
+ //let a = prompt("a sonini kiriting");
+ //let b = prompt("b soninikiriting");
+ //let c = prompt("c sonini kiriting");
+
+ //V = a * b * c;
+ //S = 2 * (a * b + b * c + a * c);
+
+ //console.log(V, S);
+
+ //5-masala
+
+// let a =prompt("a sonini kiriting");
+ //let b =prompt("b sonini kiriting");
+
+ //M = (a + b) / 2;
+
+ //console.log(M);
+ 
+//6-masala
+//let a = prompt("a sonini kiriting");
+//let b = prompt("b sonini kiriting");
+
+//c = Math.pow(a,2)+Math.pow(b,2);
+//P = a + b + c;
+//console.log(c,P);
+ 
+//7-masala
+//let r1 = prompt("radiusni kiriting");
+//let r2 = prompt("radiusni kiriting");
+//let p = 3;
+//S1 =Math.pow(r1,2)*p;
+//S2 = Math.pow(r2,2)*p;
+//S = (S1-S2);
+//console.log(S,S1,S2);
+
+//8-masala
+
+//function aylanaHesoblash(L) {
+    //const pi = 3.14;
+
+   // const R = L / (2 * pi);
+
+    //const S = pi * R * R;
+
+  //  return { R: R, S: S };
+//}
+
+//const L = 20;
+//const natijalar = aylanaHesoblash(L);
+
+//console.log("Radius R:", natijalar.R);
+//console.log("Yuza S:", natijalar.S);
+
+//9-masala
+
+//let x1 = 5;
+//let x2 = 3;
+//let y1 = 4;
+//let y2 = 6;
+
+//L = Math.sqrt(x1-x2)**2 + (y1-y2)**2;
+//console.log(L);
+//10-masla 
+//let X = 3;
+//let Y = 2;
+//let Z = 1
+
+//let A = X;
+//let B = Y;
+//let C = Z;
+
+//π = X;
+//L = Y;
+//p = Z;
+
+//L = A;
+//π = B;
+//p = C;
+
+//console.log(C,B,A);
+//11-masala
+//let x = prompt("x sonnini kiriting");
+
+//y = 4 * (x-3) Math.pow (6) - 7 * (x-3) Math.pow (3) +2;
+
+//console.log(y);
+
+//12-masala
+//let A= 7;
+
+ //Math.pow(A,3); Math.pow(A,6); Math.pow(A,9); Math.pow(A,15);
+
+//console.log(A);
+//13-masala
+//let Tf =prompt("farangeytni kiriting");
+
+//Tc = (Tf - 32) * 5/9;
+
+//console.log(Tc);
+//14-masala
+//let Xkg =prompt("kiloggramni kiriting");
+//let Asom = prompt("summasini kiriting");
+
+//let Ykg = prompt("ykg kiriting");
+//let Bsom = prompt("narxini kiriting");
+
+//1kg shokolad;
+
+//let w = Asom / Xkg;
+
+//let k = Bsom / Ykg;
+
+//c = w - k;
+//console.log(c);
+
+//15-masala
+
+//let X = prompt("kiloggramni kiriting");
+//let A = prompt("A somini kiriting");;
+//let Y = prompt("y kg kiriting");
+//let B = prompt("B narxini kiriting ");
+
+//let w =X * A;
+
+//let k = Y * B;
+
+//c = w * k;
+
+//console.log(c);
+  
+//16-masala
+    //let A = 321;
+
+    //c = A % 10;
+    
+    //z = Math.floor((A / 10) % 10);
+
+    //console.log("onliklar",z,"birliklar",c);
+//17-masala
+    //let A = 54678;
+    
+   // let z = A % 10; 
+   // let c = Math.floor((A / 10) % 10);
+   // let b = Math.floor(A / 100); 
+
+    
+   //d = z + c + b;
+
+   
+   // console.log(d);
+
+//18-masala
+// Uch xonali sonni teskari tartibda yozish
+//function reverseNumber(num){
+    // Sonni stringga aylantirib, teskari qilib, yana songa aylantiramiz
+  //  let reversed = num.toString().split('').reverse().join('');
+    //return parseInt(reversed);
+//}
+
+
+//let number = 765; 
+//let result = reverseNumber(number);
+
+//console.log(`Teskari son: ${result}`);
+//19-masala
+// Uch xonali sonning yuzliklar va o'nliklar raqamlarini almashtirish
+//function swapDigits(num) {
+    // Sonning yuzliklar, o'nliklar va birliklar raqamlarini ajratamiz
+    //let hundreds = Math.floor(num / 100);      // Yuzliklar
+    //let tens = Math.floor((num % 100) / 10);   // O'nliklar
+    //let ones = num % 10;                       // Birliklar
+
+    // Yuzliklar va o'nliklar raqamlarini almashtiramiz
+   // let swappedNumber = tens * 100 + hundreds * 10 + ones;
+
+  //  return swappedNumber;
+//}
+
+// Foydalanuvchidan sonni olish
+//let number = 567;  // Misol: 123 sonini teskari qilish
+//let result = swapDigits(number);
+
+//console.log(`Almashtirilgan son: ${result}`);
+
+//20-masala
+// Uch xonali sonni teskari tartibda yozish
+//function reverseNumber(num){
+    // Sonni stringga aylantirib, teskari qilib, yana songa aylantiramiz
+  //  let reversed = num.toString().split('').reverse().join('');
+    //return parseInt(reversed);
+//}
+//let number = 765; 
+//let result = reverseNumber(number);
+
+//console.log(`Teskari son: ${result}`);
+ //21-masala
+ //let a = prompt("4 xonali son kiriting");
+ //c = Math.floor(a/ 100);
+ //let R = (a % 10);
+ //console.log(R);
+ // 22-masala
+ 
+  //function secondsToMinutes(n) {
+   
+    //let fullMinutes = Math.floor(n / 60);
+
+  //  return fullMinutes;
+//}
+
+
+//let n = 5000;
+//let result = secondsToMinutes(n);
+
+//console.log( n );
+
+//1-masala
+//let A = prompt("A sonini kiriting:");
+
+//if (A > 0) {
+  //console.log(true);
+//} else {
+  //console.log(false);
+//}
+//2-masala 
+//let A = prompt("A sonini kiriting");
+
+//if(A % 2 !== 0){
+  //console.log(true);
+//} else{
+  //console.log(false);
+  
+//}
+//3-masala
+//let A = prompt("A sonini kiriting");
+//let B = prompt("B sonini kiriting");
+
+ //if("A>2 va B <=3"){
+  //console.log(true);
+// }
+ //else{
+  //console.log(false);
+// }
+//4-masala
+//let A = prompt("A sonini kiriting");
+//let B = prompt("B sonini kiriting");
+//let C = prompt("C sonini kiriting");
+
+//if (A <= B <= C){
+  //console.log(true);
+//}
+//else{
+ // console.log(false);
+//}
+//5-masala 
+//let A = prompt("A sonining qiymatini kiritish");
+//let B = prompt(" B sonining qiymatini kiritish ");
+
+//if (A % 2 === 0 && B % 2 === 0)(A % 2 !== 0 && B % 2 !== 0) 
+//{
+  //console.log(true);
+//}
+//else{
+  //console.log(false);
+//}
+//6-masala 
+///let A = prompt("A sonini kiriting");
+//let B = prompt("B sonini kiriting");
+//let C = prompt("C sonini kiriting");
+//if (A > 0 || B > 0 || C > 0){
+//  console.log(true);
+//}
+//else{
+  //console.log(false);
+//}
+//7-masala 
+//let A = prompt("A sonni kiriting");
+//if(A < 100 || A > 999){
+  //console.log(true); 
+//}
+//else{ 
+//console.log(false);
+//}
+  
+//8-masala
+//let A = prompt("A sonini kiriting");
+//let B = prompt("B sonini kiriting");
+//let C = prompt("C sonini kiriting");
+//if( A === B || A === C || B === C){
+  //console.log(true);
+//}
+//else{
+  //console.log(false);
+//}
+//9-masala 
+
+//let dx = parseInt(prompt("dx sonini kiriting"), 10);
+//let dy = parseInt(prompt("dy sonini kiriting"), 10);
+
+//if ((dx === 2 && dy === 1) || (dx === 1 && dy === 2)) {
+  //  console.log(true);
+//} else {  
+  //  console.log(false);
+//}
+//Uyga vazifa 
+//1-masala
+//let A = prompt("A sonini kiriting");
+//if(A % 2 === 0){
+  //console.log(true);
+//}
+//else{
+  //console.log(false);
+//}
+//2-masala
+//let A = prompt("A sonini kiriting");
+//let B = prompt("B sonini kiriting");
+//if(A >= 0 || B < -2){
+  //console.log(true);
+//}
+//else{
+  //console.log(false);
+//}
+//3-masala\
+//let A = prompt("A sonini kiriting");
+//let B = prompt("B sonini kiriting");
+//let C = prompt("C sonini kiriting");
+
+//if((A < B && B < C) || (C < B && B < A)){
+  ///console.log(true);
+//}else{
+//console.log(false);
+//}
+//4-masala
+//let A = prompt("A sonini kiriting");
+//let B = prompt("B sonini kiriting");
+//if(A % 2 !== 0 && B % 2 !== 0){
+  //console.log(true);
+//}else{
+  //console.log(false);
+//}
+ //5-masala
+// let A = prompt("A sonini kiriting");
+ //let B = prompt("B sonini kiriting");
+
+//if( A % 2 !== 0 || B % 2 !== 0){
+  //console.log(true);
+//}else{
+  //console.log(false);
+//}
+//6-masala
+//let A = prompt("A sonini kiriting");
+//let B = prompt("B sonini kiriting");
+//if((A % 2 !== 0) !== (B % 2 !== 0)){
+  //console.log(true);
+//}else{
+  //console.log(false);
+//}
+//7-masala
+//let A = prompt("A sonini kiriting");
+//let B = prompt("B sonini kiriting");
+//let C = prompt("C sonini kiriting");
+//if(A > 0 && B > 0 && C > 0){
+  //console.log(true);
+//}
+//else{
+  //console.log(false);
+//}
+//8-masala
+//let A = prompt("A sonini kiriting");
+//let B = prompt("B sonini kiriting");
+//let C = prompt("C sonini kiriting");
+//if((A > 0) + (B > 0) + (C > 0) === 1){
+  //console.log(true);
+//}else{
+  //console.log(false);
+//}
+//9-masala
+//let A = prompt("A sonini kiriting");
+//let B = prompt("B sonini kiriting");
+//let C = prompt("C sonini kiriting");
+//if((A > 0) + (B > 0) + (C > 0) === 2){
+  //console.log(true);
+//}else{
+  //console.log(false);
+//}
+//10-masala
+//let A = prompt("A sonini kiriting");
+//if(A > 9 && A < 100 && A % 2 === 0){
+  //console.log(true);
+//}else{
+  //console.log(false);
+//}
+//11-masala
+//let A = prompt("A sonini kiriting");
+//(A > 99 && A < 1000 && A % 2 !== 0){
+  //console.log(true);
+//}else{
+ // console.log(false);
+//}
+//12-masala
+//let A = prompt("A sonini kiriting");
+//let B = prompt("B sonini kiriting");
+//let C = prompt("C sonini kiriting");
+//if((A > 0 && B < 0) || (A > 0 && C < 0) || (B > 0 && A < 0) || (B > 0 && C < 0) || (C > 0 && A < 0) || (C > 0 && B < 0)){
+  //console.log(true);
+//}else{
+  //console.log(false);  
+//}
+//13-masala
+//let A = prompt("A sonini kiriting");
+  //  let digits = num.toString().split('').map(Number);
+  //if(A[0] < A[1] && A[1] < A[2]){
+    //console.log(true);
+  //}else{
+    //console.log(false);
+  //}
+//14-masala
+//let A = prompt("A sonini kiriting");
+//let digits = num.toString().split('').map(Number);
+//let increasing = A[0] < A[1] && A[1] < A[2];
+//let decreasing = A[0] > A[1] && A[1] > A[2];
+//if( increasing || decreasing){
+//  console.log(true);
+//}else{
+  //console.log(false);
+//}
+//16-masala
+//let x1 = prompt("x1 sonini kiriting");
+//let x2 = prompt("x2 sonini kiriting");
+//let y1 = prompt("y1 sonini kiriting");
+//let y2 = prompt("y2 sonini kiriting");
+//if((x1 + y1) % 2 === (x2 + y2) % 2){
+  //console.log(true);
+//}else{
+  //console.log(false);
+//}
+//17-masala
+//let x1 = prompt("x1 sonini kiriting");
+//let x2 = prompt("x2 sonini kiriting");
+//let y1 = prompt("y1 sonini kiriting");
+//let y2 = prompt("y2 sonini kiriting");
+//if(Math.abs(x1 - x2) <= 1 && Math.abs(y1 - y2) <= 1){
+  //console.log(true);
+//}else{
+  //console.log(false);
+//}
+//18-masala
+//let x1 = prompt("x1 sonini kiriting");
+//let x2 = prompt("x2 sonini kiriting");
+//let y1 = prompt("y1 sonini kiriting");
+//let y2 = prompt("y2 sonini kiriting");
+//if(x1 === x2 || y1 === y2 || Math.abs(x1 - x2) === Math.abs(y1 - y2)){
+  //console.log(true);
+//}else{
+  //console.log(false);
+//}
+//19-masala
+//let x1 = prompt("x1 sonini kiriting");
+//let x2 = prompt("x2 sonini kiriting");
+//let y1 = prompt("y1 sonini kiriting");
+//let y2 = prompt("y2 sonini kiriting");
+//if(Math.abs(x1 - x2) === Math.abs(y1 - y2)){
+  //console.log(true);
+//}else{
+  //console.log(false);
+//}
+//20-masala
+//let x1 = prompt("x1 sonini kiriting");
+//let x2 = prompt("x2 sonini kiriting");
+//let y1 = prompt("y1 sonini kiriting");
+//let y2 = prompt("y2 sonini kiriting");
+//if((Math.abs(x1 - x2) === 2 && Math.abs(y1 - y2) === 1) || (Math.abs(x1 - x2) === 1 && Math.abs(y1 - y2) === 2)){
+  //console.log(true);
+//}else{
+  //console.log(false);  
+//}
